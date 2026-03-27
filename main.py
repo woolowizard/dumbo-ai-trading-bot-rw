d9939dfbf2c618bafb3070c2bc88afd092c83ae9
woolowizard <andreacommodi17@gmail.com>
2026-03-27 19:56:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,15 @@
 # ============================================================
 
 from src.trading_bot.main_loop import run
+from src.trading_bot.config import settings
+from alpaca.trading.client import TradingClient
 
 if __name__ == '__main__':
-    run()
+    
+    client = TradingClient(settings.api.api_key, settings.api.api_secret)
+    clock = client.get_clock()
+    
+    if clock.is_open:
+        run()
+    
+    
